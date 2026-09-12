e1eac45178831e4a99317647f5afd36da30bda6d
EyesMiror <eyesmiror@example.com>
2026-09-12 16:33:46 +0100

    Add user API

--- web.js
+++ web.js
@@ -3,10 +3,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/api/user", (req, res) => {
+    res.json({
+        id: req.query.id || null,
+        balance: 0,
+        friends: 0,
+        level: 1,
+        energy: 1000
+    });
 });
 
 app.listen(PORT, () => {
